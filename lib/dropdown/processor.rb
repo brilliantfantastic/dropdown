051505ef13aa197b067910fb6e08eab133440de7
Jamie Wright <jamie@brilliantfantastic.com>
2013-05-02 00:29:16 -0400

    Speced out general structure for the process method

--- lib/dropdown/processor.rb
+++ lib/dropdown/processor.rb
@@ -1,9 +1,16 @@
 module Dropdown
   class Processor
-    def initialize(source)
+    attr_accessor :markdown_iterator,
+                  :output_store,
+                  :renderer
+
+    def initialize
     end
 
     def process
+      markdown_iterator.each do |file|
+        output_store.save renderer.render file
+      end
     end
   end
 end
--- spec/dropdown/processor_spec.rb
+++ spec/dropdown/processor_spec.rb
@@ -2,14 +2,46 @@ require_relative '../../lib/dropdown'
 
 describe Dropdown::Processor do
   describe '.process' do
-    let(:source) { 'data/home_dir/markdown' }
+    let(:iterator) { double }
+    let(:renderer) { double }
+    subject { Dropdown::Processor.new }
 
     it 'loops through each file in a source directory' do
-      Dropdown::Processor.new(source).process
+      subject.markdown_iterator = iterator
+
+      iterator.should_receive(:each).once
+
+      subject.process
     end
 
-    it 'creates a new html file in the destination directory for each file in the source directory'
-    it 'stores the title in the index with each new file'
+    context 'with some files' do
+      let(:files) { [double, double] }
+      let(:store) { double }
+
+      it 'loops through each file and renders HTML' do
+        iterator.should_receive(:each).and_yield(files[0]).and_yield(files[1])
+        subject.markdown_iterator = iterator
+        store.as_null_object
+        subject.output_store = store
+
+        subject.renderer = renderer
+
+        files.each { |file| renderer.should_receive(:render).with file }
+        subject.process
+      end
+
+      it 'stores the rendered file' do
+        iterator.should_receive(:each).and_yield(files[0]).and_yield(files[1])
+        subject.markdown_iterator = iterator
+        renderer.as_null_object
+        subject.renderer = renderer
+
+        subject.output_store = store
+
+        store.should_receive(:save).exactly(files.count).times
+        subject.process
+      end
+    end
 
     context 'updating an existing markdown file' do
     end
