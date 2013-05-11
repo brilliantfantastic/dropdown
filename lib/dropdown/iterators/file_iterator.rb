a6934d72953f0424853f4e0c24a054b4fd35044e
Jamie Wright <jamie@brilliantfantastic.com>
2013-05-11 16:25:28 -0400

    Implemented specs from file iterator

--- lib/dropdown/iterators/file_iterator.rb
+++ lib/dropdown/iterators/file_iterator.rb
@@ -1,6 +1,16 @@
 module Dropdown
   module Iterators
     class FileIterator
+      def initialize(source)
+        raise ArgumentError if source.nil?
+        @source = source.sub(/(\/)+$/, '')
+      end
+
+      def each
+        Dir.glob("#{@source}/**/*.{md,markdown}") do |markdown_file|
+          yield markdown_file
+        end
+      end
     end
   end
 end
--- spec/dropdown/iterators/file_iterator_spec.rb
+++ spec/dropdown/iterators/file_iterator_spec.rb
@@ -1,14 +1,42 @@
+require 'tempfile'
+require 'pathname'
 require_relative '../../../lib/dropdown/iterators/file_iterator'
 
 describe Dropdown::Iterators::FileIterator do
   describe '.initialize' do
-    it 'should require a source path'
+    it 'should require a source path' do
+      expect{ Dropdown::Iterators::FileIterator.new(nil) }.to raise_error ArgumentError
+    end
   end
 
   describe '.each' do
-    it 'should iterate over markdown files that end in .md'
-    it 'should iterate over markdown files that end in .markdown'
-    it 'should ignore files that are not markdown files'
+    it 'should iterate over markdown files that end in .md' do
+      file = Tempfile.new ['test', '.md']
+      dir = Pathname.new(file.path).dirname
+      expect{ |f| Dropdown::Iterators::FileIterator.new(dir).each(&f) }.to yield_with_args file.path
+      file.delete
+    end
+
+    it 'should iterate over markdown files that end in .markdown' do
+      file = Tempfile.new ['test', '.markdown']
+      dir = Pathname.new(file.path).dirname
+      expect{ |f| Dropdown::Iterators::FileIterator.new(dir).each(&f) }.to yield_with_args file.path
+      file.delete
+    end
+
+    it 'should ignore files that are not markdown files' do
+      file = Tempfile.new 'test'
+      dir = Pathname.new(file.path).dirname
+      expect{ |f| Dropdown::Iterators::FileIterator.new(dir).each(&f) }.not_to yield_control
+      file.delete
+    end
+
+    it 'should handle an extra slash in the source' do
+      file = Tempfile.new ['test', '.markdown']
+      dir = "#{Pathname.new(file.path).dirname}/"
+      expect{ |f| Dropdown::Iterators::FileIterator.new(dir).each(&f) }.to yield_with_args file.path
+      file.delete
+    end
   end
 end
 
