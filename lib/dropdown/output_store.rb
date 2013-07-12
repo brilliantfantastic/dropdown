193beb1c33491903bc88ffefd845818c0abdc017
Keith Thompson <keith@brilliantfantastic.com>
2013-07-12 15:17:30 -0400

    Implemented the create output directory spec

--- lib/dropdown/output_store.rb
+++ lib/dropdown/output_store.rb
@@ -2,14 +2,20 @@ module Dropdown
   class OutputStore
     attr_reader :path
 
-    def initialize(initial_path)
-      @initial_path = initial_path
+    def initialize(output_path)
+      @output_path = output_path
     end
 
     def save(html)
-      #TODO: Filename should be MD5 hash?
-      @path = "#{@initial_path}/test.html"
+      create_directory @output_path
+      @path = "#{@output_path}/test.html"
       File.open(@path, 'w+') { |f| f.write html }
+    end
+
+    private
+
+    def create_directory(path)
+      FileUtils.mkdir_p(path)
     end
   end
 end
--- spec/dropdown/output_store_spec.rb
+++ spec/dropdown/output_store_spec.rb
@@ -4,15 +4,16 @@ require_relative '../../lib/dropdown/output_store'
 
 describe  Dropdown::OutputStore do
   let(:html) { "<p>This is <em>great</em>, yo</p>\n" }
-  subject { Dropdown::OutputStore.new Dir.tmpdir }
+  let(:output_path) { File.join(Dir.tmpdir, 'foo') }
+  subject { Dropdown::OutputStore.new output_path }
 
   describe '#save' do
-    it 'should store a file' do
+    it 'create an output directory if it does not exist' do
       subject.save html
       Pathname.new(subject.path).should exist
     end
 
-    it 'should not create an output directory if one exists'
-    it 'should save the html file in the directory'
+    it 'names the output file the same as the input file'
+    it 'saves the html file in the directory'
   end
 end
