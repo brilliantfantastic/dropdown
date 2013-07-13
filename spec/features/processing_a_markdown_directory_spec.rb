11a4ef3dd378a8b9cb0a0ef6b6da0cd31350e325
Jamie Wright <jamie@brilliantfantastic.com>
2013-07-12 22:32:36 -0400

    Completed the feature to ensure that the html files are generated

--- spec/features/processing_a_markdown_directory_spec.rb
+++ spec/features/processing_a_markdown_directory_spec.rb
@@ -5,17 +5,19 @@ describe 'Processing a directory of markdown files' do
 
     let(:current_directory) { File.expand_path File.dirname(__FILE__) }
     let(:blog_directory) { File.join current_directory, '../fixtures/blog' }
-    let(:html_directory_path) { File.join(blog_directory, 'html') }
+    let(:html_directory) { File.join(blog_directory, 'html') }
 
     subject { Dropdown::Processor.new }
 
+    after { FileUtils.rm_rf("#{html_directory}") }
+
     it 'generates the html files for the markdown files' do
       subject.source = blog_directory
-      subject.output_store = Dropdown::OutputStore.new(html_directory_path)
+      subject.output_store = Dropdown::OutputStore.new(html_directory)
       subject.renderer = :markdown_renderer
       subject.process
 
-      html_files = Dir[File.join(html_directory_path, '**', '*')]
+      html_files = Dir[File.join(html_directory, '**', '*')]
       html_files.length.should be > 0
     end
 
