3067fd750794d004734c77ae66649eeb56486fb7
Jamie Wright <jamie@brilliantfantastic.com>
2013-08-16 22:18:30 -0400

    Include metadata parser in requires

--- lib/dropdown/post.rb
+++ lib/dropdown/post.rb
@@ -2,8 +2,27 @@ module Dropdown
   class Post
     attr_reader :source
 
+    def title
+      metadata[:title]
+    end
+
     def initialize(source)
       @source = source
+    end
+
+    private
+
+    def metadata
+      unless @metadata
+        @metadata = Dropdown::Parsers::MetadataParser.new(file)
+        @metadata.parse
+      end
+      @metadata.headers
+    end
+
+    def file
+      @file = File.new(@source, 'r') unless @file
+      @file
     end
   end
 end
