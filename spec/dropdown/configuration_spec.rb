95eca2131d437e20ec848ab16171bd47e4817e59
Jamie Wright <jamie@brilliantfantastic.com>
2013-08-24 14:03:36 -0400

    Added dropdown configuration variables

--- spec/dropdown/configuration_spec.rb
+++ spec/dropdown/configuration_spec.rb
@@ -5,5 +5,8 @@ describe Dropdown::Configuration do
   subject { Dropdown::Configuration.new }
   it 'sets defaults' do
     subject.renderer.should == :markdown_renderer
+    subject.dropdown_app_key.should be_nil
+    subject.dropdown_app_secret.should be_nil
+    subject.dropdown_access_token.should be_nil
   end
 end
