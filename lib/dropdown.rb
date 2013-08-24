1c15f551cd22795a7032f7cd6281ee9def2720b0
Jamie Wright <jamie@brilliantfantastic.com>
2013-08-24 13:58:53 -0400

    Tests configure block for Dropdown

--- lib/dropdown.rb
+++ lib/dropdown.rb
@@ -7,3 +7,13 @@ require_relative 'dropdown/parsers/excerpt_extractor'
 require_relative 'dropdown/markdown_renderer'
 require_relative 'dropdown/blog'
 require_relative 'dropdown/post'
+
+module Dropdown
+  def self.configuration
+    @configuration ||= Dropdown::Configuration.new
+  end
+
+  def self.configure
+    yield configuration if block_given?
+  end
+end
