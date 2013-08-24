5805a7141dea784ec3bd4fd73f16986640554154
Jamie Wright <jamie@brilliantfantastic.com>
2013-08-24 14:31:31 -0400

    Dropbox configuration changed from dropdown

--- lib/dropdown/configuration.rb
+++ lib/dropdown/configuration.rb
@@ -1,9 +1,9 @@
 module Dropdown
   class Configuration
     attr_accessor :renderer,
-                  :dropdown_app_key,
-                  :dropdown_app_secret,
-                  :dropdown_access_token
+                  :dropbox_app_key,
+                  :dropbox_app_secret,
+                  :dropbox_access_token
 
     def initialize
       @renderer = :markdown_renderer
--- spec/dropdown/configuration_spec.rb
+++ spec/dropdown/configuration_spec.rb
@@ -5,8 +5,8 @@ describe Dropdown::Configuration do
   subject { Dropdown::Configuration.new }
   it 'sets defaults' do
     subject.renderer.should == :markdown_renderer
-    subject.dropdown_app_key.should be_nil
-    subject.dropdown_app_secret.should be_nil
-    subject.dropdown_access_token.should be_nil
+    subject.dropbox_app_key.should be_nil
+    subject.dropbox_app_secret.should be_nil
+    subject.dropbox_access_token.should be_nil
   end
 end
