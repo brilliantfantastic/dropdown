759b8330214ab74b85a895cdd7bcabaf5b46114f
Jamie Wright <jamie@brilliantfantastic.com>
2013-08-27 22:13:17 -0400

    Changed the Metadata parser to use content instead of a file

--- spec/dropdown/parsers/metadata_parser_spec.rb
+++ spec/dropdown/parsers/metadata_parser_spec.rb
@@ -1,4 +1,3 @@
-require 'tempfile'
 require_relative '../../spec_helper'
 require_relative '../../../lib/dropdown/parsers/parser'
 require_relative '../../../lib/dropdown/parsers/metadata_parser'
@@ -7,9 +6,7 @@ describe Dropdown::Parsers::MetadataParser do
   describe '.initialize' do
 
     context 'setting instance variables' do
-      let(:file) { Tempfile.new ['text', '.html'] }
-
-      subject { Dropdown::Parsers::MetadataParser.new(file) }
+      subject { Dropdown::Parsers::MetadataParser.new('Hello world') }
 
       it 'defaults the headers to an empty hash' do
         subject.headers.should == {}
@@ -18,13 +15,14 @@ describe Dropdown::Parsers::MetadataParser do
   end
 
   describe '#parse' do
-    let(:file) {
+    let(:contents) {
       file_path = File.join(File.dirname(__FILE__),
                             '../../fixtures/processed/my-trip-to-africa.html')
-      File.new(file_path, 'r')
+      File.new(file_path, 'r').readlines.join
     }
 
-    subject { Dropdown::Parsers::MetadataParser.new(file) }
+    subject { Dropdown::Parsers::MetadataParser.new(contents) }
+
     it 'creates a header variable for each comment line prior to the body' do
       subject.parse
       subject.headers.should == {
